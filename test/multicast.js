98c5ae3b7777ec1c066bd8d7b16872f269b73285
mesmo <mesmo@steelbreeze.net>
2017-07-22 11:08:15 +0100

    Add nyc to package dependencies

--- test/multicast.js
+++ test/multicast.js
@@ -3,24 +3,24 @@ var assert = require("assert"),
 	delegate = require("../lib/node/delegate");
 
 var i = 0;
-var d1 = delegate.create(function() { i += 1; }, function() { i += 2; });
-var d2 = delegate.create(function() { i += 4; }, function() { i += 8; });
+var d1 = delegate.create(function(j) { i += j; }, function(j) { i += j; });
+var d2 = delegate.create(function(j) { i += j; }, function(j) { i += j; });
 var d3 = delegate.create(d1, d2);
 
 describe("test/multicast.js", function () {
 	it("multicast delegates call all delegates", function () {
 		assert.equal(0, i);
 
-		d1();
+		d1(1);
 
-		assert.equal(3, i);
+		assert.equal(2, i);
 
-		d2();
+		d2(2);
 
-		assert.equal(15, i);
+		assert.equal(6, i);
 
-		d3();
+		d3(3);
 
-		assert.equal(30, i);
+		assert.equal(18, i);
 	});
 });
